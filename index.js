2fc15bd898f9b4a632c7c384f4236d1775f60392
collins muriuki <muriukicollins48@gmail.com>
2025-03-06 09:01:45 +0300

    declared  variables

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+let random = 0;
+++random;
+let num3 = 24;  // Dividend
+let num4 = 5;   // Divisor
+let mod = num3 % num4;  // This will give the remainder
+
+console.log(mod);  // Output: 4
+let max = Math.max(15, 20, 7, 9, 10);  // Finds the highest number
+
+console.log(max);  // Output: 20
